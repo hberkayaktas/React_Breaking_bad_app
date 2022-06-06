3acdc99927a02dd25b9d1a36733c59c1125c29a5
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-06-07 02:00:06 +0300

    react router added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react-dom";
+import { BrowserRouter, Routes, Route, Link  } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <nav>
+      <h1>NAV</h1>
+      <ul className="list">
+        <li>
+          <Link className={({ isActive }) => "nav-link" + (isActive ? " active" : "")} to="/">Home</Link>
+        </li>
+        <li>
+          <Link className={({ isActive }) => "nav-link" + (isActive ? " active" : "")} to="/about">About</Link>
+        </li>
+        <li>
+          <Link className={({ isActive }) => "nav-link" + (isActive ? " active" : "")} to="/users">Users</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<> Error </>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
+function Home() {
+  return (
+    <div>
+     home
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+     about
+    </div>
+  );
+}
 export default App;
