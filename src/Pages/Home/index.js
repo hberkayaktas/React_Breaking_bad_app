ffa6d32184baf1b91e2a50644236f61d430b2fdb
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-06-11 03:10:09 +0300

    character listing added

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,17 +1,46 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCharacters } from "../../redux/charactersSlice";
+import Masonry from 'react-masonry-css';
+import './styles.css';
 
 function Home() {
-  const data = useSelector((state) => state.characters);
+  const characters = useSelector((state) => state.characters.persons);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const error = useSelector((state) => state.characters.error);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
-  console.log(data);
-  
-  return <div>Home</div>;
+  //console.log(data);
+  if(isLoading){
+    return <div>Loading</div>
+  }
+  if(error){
+    return <div>error</div>
+  }
+  return (
+    <div>
+      <h1>Characters</h1>
+      <Masonry
+        breakpointCols={4}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {characters.map((character) => (
+          <div key={character.char_id}>
+            <img
+              src={character.img}
+              alt={character.name}
+              className="character"
+            />
+            <h4>{character.name}</h4>
+          </div>
+        ))}
+      </Masonry>
+    </div>
+  );
 }
 
 export default Home;
--- src/redux/charactersSlice.js
+++ src/redux/charactersSlice.js
@@ -1,20 +1,29 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-
+const char_limit = 12;
 export const fetchCharacters = createAsyncThunk('characters/getCharacters',async () => {
-      const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`);
+      const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=${char_limit}`);
       return res.data;
 })
 export const charactersSlice = createSlice ({
       name:'characters',
       initialState: {
             persons:[],
+            isLoading: false,
       },
       reducers:{},
       extraReducers:{
+            [fetchCharacters.pending]: (state,action) =>{
+                  state.isLoading = true;
+            },
             [fetchCharacters.fulfilled]:(state,action) => {
-                  console.log(action.payload);
+                  state.persons = action.payload;
+                  state.isLoading =false;
+            },
+            [fetchCharacters.rejected]:(state,action) => {
+                  state.isLoading =false;
+                  state.error = action.error.message;
             },
       },
 });
